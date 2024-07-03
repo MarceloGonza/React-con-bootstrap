2bdcca59b9f0eba35debe666024daded9fef6afa
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2024-07-03 11:41:22 -0300

    List agregado

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,13 @@
 // las etiquetas que parecen html son en realidad etiquetas jsx
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
+  const list = ["Goku", "Vegeta", "Picollo"];
   return (
     <Card>
       <CardBody title="Hola mundo" text="Este es el texto" />
+      <List data={list} />
     </Card>
   );
 }
