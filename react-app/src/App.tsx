e65ea61d12d932bc71193106a827c03c73f027a3
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2024-07-10 12:02:35 -0300

    boton de carga

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,29 @@
 // las etiquetas que parecen html son en realidad etiquetas jsx
 import Card, { CardBody } from "./components/Card";
+import Button from "./components/Button";
 import List from "./components/List";
+import { useState } from "react";
 
 function App() {
-  const list: string[] = [];
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
+  const list: string[] = ["Uno", "Dos", "Tres"];
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
 
-  const contenido = list.length !== 0 && (
-    <List data={list} onSelect={handleSelect} />
-  );
-
   return (
     <Card>
       <CardBody title="Hola mundo" text="Este es el texto" />
-      {contenido}
+      {list.length !== 0 ? (
+        <List data={list} onSelect={handleSelect} />
+      ) : (
+        "No hay contenido"
+      )}
+      <Button isLoading={isLoading} onClick={handleClick}>
+        Hola mundo
+      </Button>
     </Card>
   );
 }
