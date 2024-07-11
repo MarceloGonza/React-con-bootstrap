e9eed9f72358bd1278df3ffde87d76a4de8b01c6
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2024-07-11 11:34:43 -0300

    ejercicio boton de agregado

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,25 +5,35 @@ import List from "./components/List";
 import { useState } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const handleClick = () => setIsLoading(!isLoading);
+  const [data, setData] = useState(["Uno", "Dos", "Tres"]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const handleClick = () => setIsLoading(!isLoading);
 
-  const list: string[] = ["Uno", "Dos", "Tres"];
-  const handleSelect = (elemento: string) => {
-    console.log("imprimiendo", elemento);
-  };
+  // const list: string[] = ["Uno", "Dos", "Tres"];
+  // const handleSelect = (elemento: string) => {
+  //   console.log("imprimiendo", elemento);
+  // };
 
+  // return (
+  //   <Card>
+  //     <CardBody title="Hola mundo" text="Este es el texto" />
+  //     {list.length !== 0 ? (
+  //       <List data={list} onSelect={handleSelect} />
+  //     ) : (
+  //       "No hay contenido"
+  //     )}
+  //     <Button isLoading={isLoading} onClick={handleClick}>
+  //       Hola mundo
+  //     </Button>
+  //   </Card>
+  // );
+  const addMinion = () => setData([...data, "Minion"]);
+  const remMinion = () => setData(data.slice(0, -1));
   return (
     <Card>
-      <CardBody title="Hola mundo" text="Este es el texto" />
-      {list.length !== 0 ? (
-        <List data={list} onSelect={handleSelect} />
-      ) : (
-        "No hay contenido"
-      )}
-      <Button isLoading={isLoading} onClick={handleClick}>
-        Hola mundo
-      </Button>
+      <Button onClick={addMinion}>Agregar</Button>
+      <Button onClick={remMinion}>Eliminar</Button>
+      <List data={data} />
     </Card>
   );
 }
