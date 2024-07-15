1cb527eef64aeda5a8a21db6abec00a23233f7e9
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2024-07-15 11:22:20 -0300

    aplicando css con module

--- react-app/src/components/Button/index.tsx
+++ react-app/src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import styles from "./Button.module.css";
 
 type Props = {
   children: ReactNode;
@@ -7,12 +8,18 @@ type Props = {
 };
 
 function Button({ children, isLoading, onClick }: Props) {
+  const className = [
+    `btn btn-${isLoading ? "secondary" : "primary"}`,
+    styles.button,
+  ].join(" ");
+
   return (
     <button
       onClick={onClick}
       disabled={isLoading}
       type="button"
-      className={`btn btn-${isLoading ? "secondary" : "primary"}`}
+      //className={[styles.button, styles.padded].join(" ")}
+      className={className}
     >
       {isLoading ? "Cargando..." : children}
     </button>
